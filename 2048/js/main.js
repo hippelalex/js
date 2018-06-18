97ad9efcfef2974a9e34aeea6119a967dd99573d
Alexander <hippelalex@gmail.com>
2018-06-18 15:57:55 +0300

    2048 updates

--- 2048/js/main.js
+++ 2048/js/main.js
@@ -1,4 +1,22 @@
 
+//get array of elements
+
+let arr = document.getElementsByClassName('block');
+console.log(arr);
+
+document.getElementById('game-container').onclick = function(event) {
+    console.log(event);
+    if (event.target.className == 'block'){
+
+            event.target.innerHTML = 2;
+
+        }
+
+}
+
+let datablock = document.getElementById('game-container').innerHTML;
+console.log(datablock);
+
 //Rendom function
 
 
@@ -10,3 +28,8 @@
 //Split function
 
 //Best score function
+
+// 0  1  2  3
+// 4  5  6  7
+// 8  9  10 11
+// 12 13 14 15
